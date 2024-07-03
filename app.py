8b2578b61d539e5716b0e4036dd5c4697c1320ec
Maciej Fularz <fularzmaciej@gmail.com>
2024-07-03 20:15:01 +0200

    Added styling, footer and function to inject curr year to templates

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
+
 
 @app.route('/')
 def index():
@@ -20,3 +26,4 @@ def projects():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
